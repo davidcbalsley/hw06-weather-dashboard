fcc461f1abadee94a851f2fbe029cd744d367d66
David Joseph <56838866+davidcbalsley@users.noreply.github.com>
2020-02-03 11:43:10 -0600

    displaying more of current weather conditions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document.readyState(function() {
+$(document).ready(function() {
 
     // Pseudocode!
 
@@ -31,34 +31,83 @@ $(document.readyState(function() {
     // On page load
     // - Load cities from local storage, create buttons for them, display them
 
+    // What to re-use
+    // - Building query URL, getting either current or 5-day forecast
+    // - Displaying current forecast
+    // - Displaying 5-day forecast
+
+    function showCurrentWeatherCondiditons(cityName, currentWeatherResponse) {
+        var currentTemp = "";       // The current temperature
+        var currentHumidity = "";   // The current humidity
+        var currentWindSpeed = "";  // The current wind speed
+
+        console.log(currentWeatherResponse); // debug
+
+        // Clear any current weather conditions
+        $("#current-temperature-result").empty();
+
+        // Get current weather conditions from response, copy to local variables
+        currentTemp = currentWeatherResponse.main.temp;
+        currentTemp = currentTemp.toFixed(1); // Show just one decimal point for temperature
+        currentHumidity = currentWeatherResponse.main.humidity;
+        currentWindSpeed = currentWeatherResponse.wind.speed;
+        currentWindSpeed = currentWindSpeed.toFixed(1); // Show just one decimal point for wind speed
+
+        // Create a div for the city name, date, and condition icon
+        var cityNameDiv = $("<div>");
+        cityNameDiv.text(cityName);
+
+        // Create a div for the current temperature
+        var currentTempDiv = $("<div>");
+        currentTempDiv.text("Temperature: " + currentTemp + " °F");
+
+        // Create a div for the current humdity
+        var currentHumidityDiv = $("<div>");
+        currentHumidityDiv.text("Humidity: " + currentHumidity + "%");
+
+        // Create a div for the current wind speed
+        var currentWindSpeedDiv = $("<div>");
+        currentWindSpeedDiv.text("Wind Speed: " + currentWindSpeed + " MPH");
+
+
+        $("#current-temperature-result").append(cityNameDiv, currentTempDiv, currentHumidityDiv, currentWindSpeedDiv);
+    }
+
+    // For the given city name, get either the current weather conditions
+    // or the 5-day forecast and return the response
+    function getCurrentOrFiveDayWeather(cityName, currentOrFiveDay) {
+        var queryURL = "";          // The query to pass to OpenWeeather
+
+        if (cityName !== null) {
+            if (currentOrFiveDay === "current") {
+                // Get the current weather conditions
+
+                // Form the queryURL
+                queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=16460027affaf1e3d6736c85f67318ba";
+            
+                // Make an AJAX call to get the current weather conditions 
+                // from OpenWeather
+                $.ajax({
+                    url: queryURL,
+                    method: "GET"
+                }).then(function(response) {
+                    // console.log(response); // debug
+                    showCurrentWeatherCondiditons(cityName, response);
+                });
+            }
+        }
+    }
+
     // Button listener for search magnifying glass button
     $("#city-search").on("click", function () {
-        var queryURL = "";
-        var cityName = "";
-        // console.log("City input = " + $("input[aria-label='city-input']").val());
+        var cityNameFromSearchButton = "";      // The namne of the city
 
-        cityName = $("input[aria-label='city-input']").val();
+        // Get the city name from the search text box
+        cityNameFromSearchButton = $("input[aria-label='city-input']").val();
 
-        // queryURL = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=YOUR_API_KEY";
-
-        queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=16460027affaf1e3d6736c85f67318ba";
-        // console.log("queryURL = " + queryURL);
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
-            console.log(response);
-
-            var currentTemp = response.main.temp;
-            var currentHumidity = response.main.humidity;
-
-            var currentTempDiv = $("<div>");
-            currentTempDiv.text("Temperature: " + currentTemp + "F");
-
-            $("#current-temperature-result").append(currentTempDiv);
-  
-        });
-    })
-
+        // If the user entered a city in the search text box, get that city's current and 5-day weather
+        if (cityNameFromSearchButton) {  
+            getCurrentOrFiveDayWeather(cityNameFromSearchButton, "current");
+        }
+    });
 });
